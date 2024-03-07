0d119faefdb5ae90009d3920b50fcd703dc85acf
PAN <paularthur@navaud.fr>
2024-03-07 16:48:18 +0100

    Second Commit

---
+++ src/test/java/com/banque/banqueconfigserver/BanqueConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banque.banqueconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
